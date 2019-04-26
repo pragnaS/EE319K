75850d5bb096ea588a1606522c885bf48541e9ad
pragnaS <36314074+pragnaS@users.noreply.github.com>
2019-04-25 22:33:41 -0500

    Update Lab8.c

--- Lab8_EE319K/Lab8.c
+++ Lab8_EE319K/Lab8.c
@@ -33,65 +33,122 @@ void EnableInterrupts(void);  // Enable interrupts
 #define PF1       (*((volatile uint32_t *)0x40025008))
 #define PF2       (*((volatile uint32_t *)0x40025010))
 #define PF3       (*((volatile uint32_t *)0x40025020))
+	
+#define ADCMail	(*((volatile uint32_t *)0x2000007D))
+//#define ADCStatus 		(*((volatile uint32_t *)0x2000008))
+
+uint32_t ADCStatus;
+
+
 // Initialize Port F so PF1, PF2 and PF3 are heartbeats
 void PortF_Init(void){
+	volatile int delay;
+	SYSCTL_RCGCGPIO_R |=0x20;
+	delay++;
+	delay++;
+	delay++;
+	GPIO_PORTF_DIR_R |=0x0E;
+	GPIO_PORTF_DEN_R |=0x0E;
+}
+
+void SysTick_Init(void){
+	
+	NVIC_ST_CTRL_R =0; //Disable SysTick during setup
+	NVIC_ST_RELOAD_R = 1333333; //maximum reload value
+	NVIC_ST_CURRENT_R = 0; //any write to current clears it
+	//NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0x00FFFFFF) | 0x20000000;
+	NVIC_ST_CTRL_R =0x07;
+}
+
+void SysTick_Handler(void){
+	GPIO_PORTF_DATA_R ^=0x04;
+	GPIO_PORTF_DATA_R ^=0x04;
+	ADCMail=ADC_In();
+	ADCStatus=1;
+	GPIO_PORTF_DATA_R ^=0x04;
 
 }
+
 uint32_t Data;        // 12-bit ADC
 uint32_t Position;    // 32-bit fixed-point 0.001 cm
-int main(void){       // single step this program and look at Data
-  TExaS_Init();       // Bus clock is 80 MHz 
-  ADC_Init();         // turn on ADC, set channel to 5
-  while(1){                
-    Data = ADC_In();  // sample 12-bit channel 5
-  }
-}
 
-int main2(void){
-  TExaS_Init();       // Bus clock is 80 MHz 
-  ADC_Init();         // turn on ADC, set channel to 5
-  ST7735_InitR(INITR_REDTAB); 
-  PortF_Init();
-  while(1){           // use scope to measure execution time for ADC_In and LCD_OutDec           
-    PF2 = 0x04;       // Profile ADC
-    Data = ADC_In();  // sample 12-bit channel 5
-    PF2 = 0x00;       // end of ADC Profile
-    ST7735_SetCursor(0,0);
-    PF1 = 0x02;       // Profile LCD
-    LCD_OutDec(Data); 
-    ST7735_OutString("    ");  // spaces cover up characters from last output
-    PF1 = 0;          // end of LCD Profile
-  }
-}
+//int main(void){       // single step this program and look at Data
+//  TExaS_Init();       // Bus clock is 80 MHz 
+//  ADC_Init();         // turn on ADC, set channel to 5
+//  while(1){                
+//    Data = ADC_In();  // sample 12-bit channel 5
+//  }
+//}
 
-// your function to convert ADC sample to distance (0.001cm)
+//int main(void){
+
+//	
+//  TExaS_Init();       // Bus clock is 80 MHz 
+//  ADC_Init();         // turn on ADC, set channel to 5
+//  ST7735_InitR(INITR_REDTAB); 
+//  PortF_Init();
+//  while(1){           // use scope to measure execution time for ADC_In and LCD_OutDec 
+//    PF2 = 0x04;       // Profile ADC
+//    Data = ADC_In();  // sample 12-bit channel 5
+//    PF2 = 0x00;       // end of ADC Profile
+//    ST7735_SetCursor(0,0);
+//    PF1 = 0x02;       // Profile LCD
+//    LCD_OutDec(Data); 
+//    ST7735_OutString("    ");  // spaces cover up characters from last output
+//    PF1 = 0;          // end of LCD Profile
+//  }
+//} 
+
+//your function to convert ADC sample to distance (0.001cm)
 uint32_t Convert(uint32_t input){
-  return 0;
-}
-int main3(void){ 
-  TExaS_Init();         // Bus clock is 80 MHz 
-  ST7735_InitR(INITR_REDTAB); 
-  PortF_Init();
-  ADC_Init();         // turn on ADC, set channel to 5
-  while(1){  
-    PF2 ^= 0x04;      // Heartbeat
-    Data = ADC_In();  // sample 12-bit channel 5
-    PF3 = 0x08;       // Profile Convert
-    Position = Convert(Data); 
-    PF3 = 0;          // end of Convert Profile
-    PF1 = 0x02;       // Profile LCD
-    ST7735_SetCursor(0,0);
-    LCD_OutDec(Data); ST7735_OutString("    "); 
-    ST7735_SetCursor(6,0);
-    LCD_OutFix(Position);
-    PF1 = 0;          // end of LCD Profile
-  }
-}   
-int realmain(void){
-  TExaS_Init();
-  // your Lab 8
-  EnableInterrupts();
-  while(1){
-  }
+  return (0.4636*input + (46.138));
 }
 
+
+//int main(void){ 
+//  TExaS_Init();         // Bus clock is 80 MHz 
+//  ST7735_InitR(INITR_REDTAB); 
+//  PortF_Init();
+//  ADC_Init();         // turn on ADC, set channel to 5
+//  while(1){  
+//    PF2 ^= 0x04;      // Heartbeat
+//    Data = ADC_In();  // sample 12-bit channel 5
+//    PF3 = 0x08;       // Profile Convert
+//    Position = Convert(Data); 
+//    PF3 = 0;          // end of Convert Profile
+//    PF1 = 0x02;       // Profile LCD
+//    ST7735_SetCursor(0,0);
+//    LCD_OutDec(Data); ST7735_OutString("    "); 
+//    ST7735_SetCursor(6,0);
+//    LCD_OutFix(Position);
+//    PF1 = 0;          // end of LCD Profile
+//  }
+//}   
+
+
+int main(void){
+  TExaS_Init();
+	ST7735_InitR(INITR_REDTAB); 
+	PortF_Init();
+	ADC_Init();
+	SysTick_Init();
+  EnableInterrupts();
+	ADCStatus =0;
+	
+	while(1){
+		
+		if(ADCStatus !=0){
+			Data =ADCMail;
+			ADCStatus = 0;
+			Position = Convert(Data);
+			ST7735_SetCursor(0,0);
+			LCD_OutDec(Data); ST7735_OutString("    "); 
+			ST7735_SetCursor(6,0);
+			LCD_OutFix(Position);
+				ST7735_SetCursor(13,0);
+				ST7735_OutString("cm");
+			
+		}
+  }
+
+}
